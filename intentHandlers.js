7cb5552a20ba603f8615398a05050a2c77ec73a9
Boaz Keren Gil <boazkerengil@Boazs-Air-2.wlan.jacobs-university.de>
2016-10-15 22:08:02 +0200

    All static implements

--- intentHandlers.js
+++ intentHandlers.js
@@ -10,18 +10,20 @@
 
 'use strict';
 var textHelper = require('./textHelper'),
-    storage = require('./storage');
+    storage = require('./storage'),
+    AlexaSkill = require('./AlexaSkill');
+
 
 var registerIntentHandlers = function (intentHandlers, skillContext) {
     intentHandlers.HitIntent = function (intent, session, response) {
         var playerName = intent.slots.PlayerName.value;
-        var player = getPlayer(playerName);
-        var playerHealth = player.health;
-        
+        //var player = getPlayer(playerName);
+        //var playerHealth = player.health;
+        var playerHealth = 10;
         if(playerHealth > 0) {
             playerHealth--;
             if(playerHealth > 0) {
-                var speechOutput = playerName + ' has been hit and has' + playerHealth + 'health points left!';
+                var speechOutput = playerName + ' has been hit and has ' + playerHealth + ' health points left!';
             } else {
                 var speechOutput = playerName + ' is dead and out of the game.';
             }
@@ -34,32 +36,43 @@ var registerIntentHandlers = function (intentHandlers, skillContext) {
     };
 
     intentHandlers.AddPlayerIntent = function (intent, session, response) {
-        var playerName = intent.slots.PlayerName.value;
-        var player = getPlayer(playerName);
-        if(Object.keys(player).length !== 0) {
-            addPlayer(playerName);
-            var speechOutput = playerName + ' has been added';
-        } else {
-            var speechOutput = playerName + ' is already in this game.';
+        var playerName1 = intent.slots.PlayerNameOne.value;
+        var playerName2 = intent.slots.PlayerNameTwo.value;
+        var playerName3 = intent.slots.PlayerNameThree.value;
+        var playerName4 = intent.slots.PlayerNameFour.value;
+        var players = [playerName1, playerName2, playerName3, playerName4];
+        var counter = 0;
+        players.forEach(function(item) {
+            if(item.length != 0) {
+                //addPlayer(this);
+                counter++; 
+                if(counter <= 2) {
+                    //addToTeam(this, "Ninja Coders");
+                } else {
+                    //addToTeam(this, "Teddy Bears");
+                }
+            }
+        });
+
+        var speechOutput = { 
+            speech: '<speak><p><s>All players have been added, lets start having fun!</s>' +
+            '<s>our teams for today are:</s><s>' + 
+            playerName1 + ' and ' +
+            playerName2 + ' are in team Ninja Coders team</s><s> and ' +
+            playerName3 + ' and ' +
+            playerName4 + ' are in Teddy Bears team.</s><s>I wish you all good luck and let the game begin!</s></p>'+
+            '<s>3<break time="1s"/>2<break time="1s"/>1<break time="1s"/>Go!</s></speak>',
+            type: AlexaSkill.speechOutputType.SSML
         }
-        
+
+        response.tell(speechOutput);
         
     };
 
-    intentHandlers.RemovePlayerIntent = function (intent, session, response) {
-        var playerName = intent.slots.PlayerName.value;
-        var speechOutput = PlayerName + ' has been hit!'
-        
-    };
 
     intentHandlers.StartGameIntent = function (intent, session, response) {
-
-        var speechOutput = PlayerName + ' has been hit!'
-        
+        response.ask('So we will start our game shortly! quick question though, who will be playing today?');        
     };
-
-
-
 
 
 
